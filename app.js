7f12f92093acee3ac5c7368098615654e49242a3
John Martin <johnjamesmartin@live.com>
2019-08-17 23:22:58 +0100

    Implement rendering of layouts

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(`${__dirname}/public`));
 /* Routes
  ****************************************************************/
 app.get('/', (req, res) => {
-  //
+  res.render('index');
 });
 
 const port = process.env.PORT || 5000;
